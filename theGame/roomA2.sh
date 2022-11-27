32b0e4c7fd26228eda3a69b0f887da93dac177c2
TraviPatty03 <TraviPatty03@users.noreply.github.com>
2022-11-27 15:14:41 -0500

    Update roomA2.sh
    Finished roomA2

--- theGame/roomA2.sh
+++ theGame/roomA2.sh
@@ -2,6 +2,11 @@
 
 #Variables
 room_A3=false
+Triangle=false
+Square=false
+Pentagon=false
+Octogon=false
+
 
 #Color Vars
 #Colors can be added but these were just some
@@ -35,7 +40,10 @@ btnPress=(
 	"${ImportantColor}Game Over${DefaultColor}"
 )
 lookAround=(
-	"Room is empty"
+	"Room is bare but a lot more coloful than the previous rooms"
+	"Straight ahead there is a door with \"A3\" written on it"
+	"On the walls there are four pictures each with a different shape"
+	"(use inspect then picture to look at the pictures)"
 )
 #########################
 #Functions
@@ -50,12 +58,11 @@ echo ""
 function RoomChange(){
 	read -p "Where do you want to go? >" selection
 	case $selection in 
-		A2)
+		A3)
 			if [[ $room_A3 == true ]]
 			then
 				echo "Next room confirmed"
 				./roomA3.sh
-				break
 			else 
 				echo "Door is still locked"
 			fi
@@ -79,7 +86,67 @@ function ButtonPressed(){
 function Inspection(){
 	read -p "What do you want to inspect? > " selection
 	case $selection in
-		desk)
+		pictures)
+			echo "There are 4 pictures, a triangle, a square, a pentagon, and an octogon"
+			read -p "Which picture do you want to look at? > " temp
+			case $temp in
+				triangle) echo "The triangle is green, there is a button under it"
+					read -p "Do you want to push it? (yes or no) > " choice
+					if [[ $choice == yes && $Square == true && $Pentagon == true && $Octogon == true ]]
+					then 
+						echo "You pushed the button"
+						room_A3=true
+						echo "There was a click at the door"
+					elif [[ $choice == yes ]]
+					then
+						Square=false
+						Pentagon=false
+						Octogon=flase
+					fi
+				;;
+				square) echo "The square is red, there is a button under it"
+					read -p "Do you want to push it? (yes or no) > " choice
+					if [[ $choice == yes && $Pentagon == true && $Octogon == true ]]
+					then 
+						echo "You pushed the button"
+						Square=true
+					elif [[ $choice == yes ]]
+					then
+						Square=false
+						Pentagon=false
+						Octogon=false
+					fi
+				;;
+				pentagon) echo "The pentagon is blue, there is a button under it"
+					read -p "Do you want to push it? (yes or no) > " choice
+					if [[ $choice == yes && $Octogon == true ]]
+					then 
+						echo "You pushed the button"
+						Pentagon=true
+					elif [[ $choice == yes ]] 
+					then 
+						Square=false
+						Pentagon=false
+						Octogon=false
+					fi
+				;;
+				octogon) echo "The octogon is black, there is a button under it"
+					read -p "Do you want to push it? (yes or no) > " choice
+					if [[ $choice == yes ]]
+					then
+						echo "You pushed the button"
+						Octogon=true
+					fi
+				;;
+				*) 
+					echo "Thats not an option"
+				;;
+			esac
+		;;
+		door)
+			echo "There is a note that reads A3, under it ther is small writing"
+			echo "\"Did you try going from greatest to least?\""
+		;;
 		*)
 			echo "Theres nothing special about that"
 		;;
@@ -110,6 +177,7 @@ while [[ $REPLY != 0 ]]; do
 		;;
 		cd) 
 			RoomChange
+			break
 		;;
 		btn)
 			ButtonPressed
